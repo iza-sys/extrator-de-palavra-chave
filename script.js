ed18cf994f1ef7c74f6fc31fd204cd993a2d42b0
iza-sys <izabella.barbosa@escola.pr.gov.br>
2025-08-13 11:23:57 -0300

    script.js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 const botaoMostraPalavras = document.querySelector('#botao-palavrachave');
 
 botaoMostraPalavras.addEventListener('click', mostraPalavrasChave);
+
+function mostraPalavrasChave() {
+const texto = document.querySelector('#entrada-de-texto')
+const campoResultado = document.querySelector('#resultado-palavrachave');
+
+  campoResultado.textContent = texto;
+} 
